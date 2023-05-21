99f68eec274016f075f259d5d617a54de5b81adb
Dmast00 <obed.hinojosa@outlook.com>
2023-05-21 15:27:14 -0600

    Add project files.

---
+++ POS.Infrastructure/Persistences/Contexts/Configuration/RoleConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using POS.Domain.Entities;
+
+namespace POS.Infrastructure.Persistences.Contexts.Configuration
+{
+    public class RoleConfiguration : IEntityTypeConfiguration<Role>
+    {
+        public void Configure(EntityTypeBuilder<Role> builder)
+        {
+            builder.HasKey(e => e.RoleId).HasName("PK__Roles__8AFACE1A83F5F0AB");
+
+            builder.Property(e => e.Description)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        }
+    }
+}
