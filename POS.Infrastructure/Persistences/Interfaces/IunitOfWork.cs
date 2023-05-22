2c8c7f50f6a5f6d70d5da31f1500cb4fd1444879
Dmast00 <obed.hinojosa@outlook.com>
2023-05-21 21:39:41 -0600

    se agregaron clases genericas, patron repository y UnitOfWork

---
+++ POS.Infrastructure/Persistences/Interfaces/IunitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS.Infrastructure.Persistences.Interfaces
+{
+    public interface IunitOfWork : IDisposable
+    {
+        //Declaracion o matricula de nuestra interfaces a nivel de repositorio
+        ICategoryRepository category { get; }
+
+        void SafeChanges();
+        Task SaveChangesAsync();
+    }
+}
---
+++ POS.Infrastructure/Persistences/Repositories/UnitOfWork.cs
@@ -0,0 +1,38 @@
+﻿using POS.Domain.Entities;
+using POS.Infrastructure.Persistences.Contexts;
+using POS.Infrastructure.Persistences.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS.Infrastructure.Persistences.Repositories
+{
+    public class UnitOfWork : IunitOfWork
+    {
+        private readonly PosContext _context; 
+        public ICategoryRepository category { get;private set; }
+
+        public UnitOfWork(PosContext context, ICategoryRepository category)
+        {
+            _context = context;
+            category  = new CategoryRepository(_context);
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+
+        public void SafeChanges()
+        {
+            _context.SaveChanges();
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
