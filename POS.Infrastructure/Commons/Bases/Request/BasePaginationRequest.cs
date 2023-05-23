8cd0839ae6a5d2c8eedf070fcd9cc9ebdb577260
Dmast00 <obed.hinojosa@outlook.com>
2023-05-22 21:23:43 -0600

    Added Filters, Methods and ICategoryRepository

--- POS.Infrastructure/Commons/Bases/Request/BasePaginationRequest.cs
+++ POS.Infrastructure/Commons/Bases/Request/BasePaginationRequest.cs
@@ -1,20 +1,21 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace POS.Infrastructure.Commons.Bases
+namespace POS.Infrastructure.Commons.Bases.Request
 {
     public class BasePaginationRequest
     {
-        public int NumPage { get; set; } =1;
-        public int NumRecordsPage { get; set; } =10;
+        public int NumPage { get; set; } = 1;
+        public int NumRecordsPage { get; set; } = 10;
         private readonly int NumMaxRecordsPage = 50;
         public string Order { get; set; } = "asc";
         public string? Sort { get; set; } = null;
-        public int Records {
+        public int Records
+        {
             get => NumRecordsPage;
             set
             {
-                NumRecordsPage = (value > NumRecordsPage) ? NumMaxRecordsPage : value;
-            } 
+                NumRecordsPage = value > NumRecordsPage ? NumMaxRecordsPage : value;
+            }
         }
 
     }
